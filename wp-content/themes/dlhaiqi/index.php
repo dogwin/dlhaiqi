e5f3a14ea283213c6b639350fc4240e62598d3ed
dogwin <weblsfamily@gmail.com>
2013-09-03 21:44:48 +0800

    add

---
+++ wp-content/themes/dlhaiqi/index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * For example, it puts together the home page when no home.php file exists.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+
+get_header();
+?>
+<?php //echo do_shortcode('[carousel]');?>
+<?php if( class_exists( 'LenSlider' ) ) {
+	//LenSlider::lenslider_output_slider( 'fb05c854ea' );
+	//LenSlider::lenslider_output_slider( '2b02aba2ad' );
+	LenSlider::lenslider_output_slider('2b02aba2ad');
+}
+?> 
+<?php get_footer();?>
